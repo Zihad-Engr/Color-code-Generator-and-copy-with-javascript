b180d251718f527c7e3258c52f0109f1678deb61
Zihad-Engr <zihadmia.eng@gmail.com>
2024-08-22 09:00:30 +0600

    now copy function is done . here one can generate color and copy that by clicking copy button

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
 let change=document.querySelector('#change');
 let body=document.querySelector('body');
-let Copy=document.querySelector('#copy');
+let Copy=document.querySelector('#Copy');
 let area=document.querySelector('#area');
 
 change.addEventListener('click',(e)=>{
     body.style.backgroundColor=ColorGenerator();
-    area.innerHTML=ColorGenerator();
+    area.value=ColorGenerator();
+});
+Copy.addEventListener('click',(e)=>{
+    navigator.clipboard.writeText(area.value);
+    alert("copied");
+    
 })
+
 
 const ColorGenerator=()=>{
     let red=Math.ceil(Math.random()*255)
@@ -18,6 +24,6 @@ return genBgColor;
 }
 
 
-console.log(ColorGenerator())
+
 
 
