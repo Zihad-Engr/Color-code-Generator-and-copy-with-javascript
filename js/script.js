479ec7354cc9cfc95c4cb566fc06be7654d663cc
Zihad-Engr <zihadmia.eng@gmail.com>
2024-08-22 09:22:24 +0600

    now Hex  and Rgb  both color Generator is done

--- js/script.js
+++ js/script.js
@@ -1,29 +1,55 @@
-let change=document.querySelector('#change');
+let changeRGB=document.querySelector('#changeRGb');
+let changeHex=document.querySelector('#changeHex');
 let body=document.querySelector('body');
 let Copy=document.querySelector('#Copy');
 let area=document.querySelector('#area');
 
-change.addEventListener('click',(e)=>{
-    body.style.backgroundColor=ColorGenerator();
-    area.value=ColorGenerator();
+
+//  RGB Genetor 
+changeRGB.addEventListener('click',(e)=>{
+    body.style.backgroundColor=ColorGeneratorRGB();
+    area.value=ColorGeneratorRGB();
 });
-Copy.addEventListener('click',(e)=>{
-    navigator.clipboard.writeText(area.value);
-    alert("copied");
-    
-})
 
-
-const ColorGenerator=()=>{
-    let red=Math.ceil(Math.random()*255)
-let green=Math.ceil(Math.random()*255)
-let blue=Math.ceil(Math.random()*255)
+const ColorGeneratorRGB=()=>{
+    let red=Math.ceil(Math.random()*255);
+let green=Math.ceil(Math.random()*255);
+let blue=Math.ceil(Math.random()*255);
 
      let genBgColor=`rgb(${red},${green},${blue})`
+
 return genBgColor;
 }
 
 
 
+// Hex Generator 
+
+changeHex.addEventListener('click',(e)=>{
+    body.style.backgroundColor=ColorGeneratorHex();
+    area.value=ColorGeneratorHex();
+});
+
+const ColorGeneratorHex=()=>{
+    let red=Math.ceil(Math.random()*255).toString(16);
+let green=Math.ceil(Math.random()*255).toString(16);
+let blue=Math.ceil(Math.random()*255).toString(16);
+     let genBgColorHex=`#${red}${green}${blue}`
+return genBgColorHex;
+}
+
+
+
+
+// Copy Code 
+
+
+Copy.addEventListener('click',(e)=>{
+    navigator.clipboard.writeText(area.value).then(function(e){
+        alert("Your Code is copy!")
+    });
+  
+    
+})
 
 
